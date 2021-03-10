279e67d79ad7d5171ce61fa156fa76a6b055d65a
popkey10 <70052055+popkey10@users.noreply.github.com>
2021-03-11 01:25:03 +0700

    เพิ่ม alert + แก้ comment

--- controllers/ac/base/Base_category.php
+++ controllers/ac/base/Base_category.php
@@ -1,9 +1,9 @@
 <?php
 /*
- * Money_category_main
+ * Base_category
  * controller หลักในการจัดการหมวดเงิน
- * @author 61160079 Adithep Phompha
- * @Create Date 2563-11-16
+ * @author 61160194 Wuttichai Chaiwanna
+ * @Create Date 2564-03-02
  */
 
 if (!defined('BASEPATH')) {
@@ -12,7 +12,7 @@ if (!defined('BASEPATH')) {
 
 include dirname(__FILE__) . '/../AC_Controller.php';
 
-class Money_category_main extends AC_Controller
+class Base_category extends AC_Controller
 {
     /*=====  contructor  ======*/
 
@@ -21,8 +21,8 @@ class Money_category_main extends AC_Controller
      * -
      * @input -
      * @output -
-     * @author 61160079 Adithep Phompha
-     * @Create Date 2563-11-16
+     * @author 61160194 Wuttichai Chaiwanna
+     * @Create Date 2564-03-02
      */
 
     public function __construct()
@@ -37,17 +37,17 @@ class Money_category_main extends AC_Controller
     /*=====  เรียกหน้า view  ======*/
 
     /*
-     * show_money_category_menu
-     * ใช้เรียกหน้าจอเมนูหมวดเงิน
+     * show_base_category
+     * ใช้เรียกหน้าจอเมนูข้อมูลพื้นฐาน
      * @input -
      * @output หน้าจอเมนูหมวดเงิน
-     * @author 61160079 Adithep Phompha
-     * @Create Date 2563-11-16
+     * @author 61160194 Wuttichai Chaiwanna
+     * @Create Date 2564-03-05
      */
 
-    public function show_money_category_menu()
+    public function show_base_category()
     {
-        $this->output_md($this->config->item('ac_v_base_folder') . "v_money_category");
+        $this->output_md($this->config->item('ac_v_base_folder') . "v_base_category");
     } // show_money_category_menu
 
     // =====================================================================================================================
@@ -61,8 +61,8 @@ class Money_category_main extends AC_Controller
      * ดึงข้อมูลหมวดเงินหลัก
      * @input -
      * @output ข้อมูลหมวดเงินหลัก
-     * @author 61160077 Siripoon Yimthanom
-     * @Update Date 2563-11-19
+     * @author 61160194 Wuttichai Chaiwanna
+     * @Update Date 2564-03-05
      */
 
     public function get_main_category_ajax()
@@ -74,13 +74,13 @@ class Money_category_main extends AC_Controller
 
         $this->bbc->bc_mt_id = 2; //2 = รายจ่าย
         $data["expense"] = $this->bbc->get_by_type()->result();
-        if(empty($data["income"])){
-            $data["income"]= "no_data";
+        if (empty($data["income"])) {
+            $data["income"] = "no_data";
         }
-        if(empty($data["expense"])){
-            $data["expense"]= "no_data";
+        if (empty($data["expense"])) {
+            $data["expense"] = "no_data";
         }
-        
+
         echo json_encode($data);
     }
 
@@ -89,8 +89,8 @@ class Money_category_main extends AC_Controller
      * เพิ่มข้อมูลหมวดเงินหลัก
      * @input ข้อมูลหมวดเงินหลัก
      * @output ข้อความแจ้งเตือนบันทึกสำเร็จ
-     * @author : 61160077 Siripoon Yimthanom
-     * @Create Date  2563-11-19
+     * @author : 61160194 Wuttichai Chaiwanna
+     * @Create Date  2564-03-05
      */
 
     public function insert_money_category_main_ajax()
@@ -128,7 +128,7 @@ class Money_category_main extends AC_Controller
      * @input ชื่อหมวดเงิน และประเภทหมวดเงิน
      * @output 0 หรือมากกว่า 0 (0 = ไม่มี, มากกว่า 0 = มี)
      * @author : 61160194 Wuttichai Chaiwanna
-     * @Create Date  2563-11-20
+     * @Create Date  2564-03-05
      */
 
     public function check_if_category_name_exist($bc_name, $bc_mt_id)
@@ -150,23 +150,49 @@ class Money_category_main extends AC_Controller
      * @input id ของหมวดเงินหลัก และหมวดเงินย่อยที่อยู่ภายใต้หมวดเงินหลักนั้น
      * @output ข้อความแจ้งเตือนลบสำเร็จ
      * @author : 61160194 Wuttichai Chaiwanna
-     * @Create Date  2563-11-20
+     * @Create Date  2564-03-05
      */
 
     public function delete_money_category_main_ajax()
     {
-        // $bc_id = $this->input->post('bc_id'); //ไอดีหมวดเงิน
-            $this->load->model($this->config->item('ac_m_folder') . 'M_ac_money_category', "bbc");
-            $this->bbc->bc_id = $this->input->post('bc_id');
-            $this->bbc->delete();
+        $bc_id = $this->input->post('bc_id'); //ไอดีหมวดเงิน
 
-            $data["json_message"] = "delete_money_category_main success";
-        
+        if ($this->check_if_category_already_use($bc_id) > 0) {
+            //กรณีถูกใช้งานอยู่
+            $data["json_data_exist"] = "Y";
+            $data["json_message"] = "delete_money_category_main_ajax cancel";
+        } //if
+        else {
+        $this->load->model($this->config->item('ac_m_folder') . 'M_ac_money_category', "bbc");
+        $this->bbc->bc_id = $this->input->post('bc_id');
+        $this->bbc->delete();
+
+        $data["json_message"] = "delete_money_category_main success";
+        }//else
         echo json_encode($data);
 
     } // delete_money_category_main_ajax
 
-        /*=====  แก้ไขข้อมูลหมวดเงินหลัก  ======*/
+    /*
+     * check_if_category_already_use
+     * ตรวจสอบว่าหมวดเงินที่ถูกส่งมาเคยถูกใช้งานอยู่หรือไม่
+     * @input bc_id
+     * @output 0 หรือมากกว่า 0 (0 = ไม่ได้ถูกใช้, มากกว่า 0 = ถูกใช้งานอยู่)
+     * @author : 61160194 Wuttichai Chaiwanna
+     * @Create Date  2564-03-05
+     */
+
+    public function check_if_category_already_use($bc_id)
+    {
+        $this->load->model($this->config->item('ac_m_folder') . 'M_ac_list_money', "bbcm");
+
+        $this->bbcm->lm_bc_id = $bc_id;
+        $found = (float) $this->bbcm->check_if_category_already_use()->row()->exist; //0 = หมวดเงินไม่ได้ถูกใช้, มากกว่า 0 = หมวดเงินถูกใช้งานอยู่
+
+        return $found;
+    } // check_if_category_already_use
+
+    /*=====  แก้ไขข้อมูลหมวดเงินหลัก  ======*/
 
     /*
      * update_money_category_main_ajax
@@ -174,7 +200,7 @@ class Money_category_main extends AC_Controller
      * @input ข้อมูลหมวดเงินหลัก
      * @output ข้อความแจ้งเตือนแก้ไขสำเร็จ
      * @author : 61160194 Wuttichai Chaiwanna
-     * @Create Date  2563-11-20
+     * @Create Date  2564-03-05
      */
 
     public function update_money_category_main_ajax()
@@ -192,7 +218,7 @@ class Money_category_main extends AC_Controller
         else {
             //กรณีไม่พบชื่อซ้ำ
             $data["json_data_exist"] = "N";
-            
+
             $this->load->model($this->config->item('ac_m_folder') . 'M_ac_money_category', "bbc");
 
             //เลขหมวดเงิน
@@ -218,5 +244,4 @@ class Money_category_main extends AC_Controller
         echo json_encode($data);
     } // update_money_category_main_ajax
 
-
-} //end class Money_category_main
+} //end class Base_category
--- models/ac/M_ac_list_money.php
+++ models/ac/M_ac_list_money.php
@@ -47,4 +47,41 @@ class M_ac_list_money extends Da_ac_list_money
         $query = $this->db->query($sql, array($this->lm_date,$this->lm_us_id,$this->lm_mt_id));
         return $query;
     } //get_by_date
+
+     /*
+     * get_by_user_id
+     * ดึงข้อมูลรายการเงินของไอดีผู้ใช้งาน
+     * @input -
+     * @output ข้อมูลทั้งหมดในตารางโดยดึงตามไอดีผู้ใช้
+     * @author 61160082 Areerat Pongurai
+     * @Create Date 2564-03-03
+     */
+
+    public function get_by_user_id()
+    {
+        $sql = "SELECT lm_bc_id,lm_us_id,lm_mt_id,lm_id,bc_name, bc_id
+				FROM {$this->db_name}.ac_list_money
+        LEFT JOIN {$this->db_name}.ac_base_category ON lm_bc_id = bc_id
+        WHERE bc_us_id=? AND bc_mt_id = ?";
+        $query = $this->db->query($sql, array($this->bc_us_id, $this->bc_mt_id));
+        return $query;
+    } //get_by_user_id
+
+    /*
+     * check_if_category_already_use
+     * ตรวจสอบว่าหมวดเงินที่อยากลบ ถูกใช้งานอยู่หรือไม่
+     * @input lm_bc_id
+     * @output 0 หรือมากกว่า 0 (0 = ไม่มี, มากกว่า 0 = มี)
+     * @author 61160194 Wuttichai Chaiwanna
+     * @Create Date 2564-03-10
+     */
+
+    public function check_if_category_already_use()
+    {
+        $sql = "SELECT COUNT(1) AS exist
+                FROM {$this->db_name}.ac_list_money
+                WHERE lm_bc_id = ?";
+        $query = $this->db->query($sql, array($this->lm_bc_id));
+        return $query;
+    } // check_if_category_already_use
 } //end class M_ac_list_money
